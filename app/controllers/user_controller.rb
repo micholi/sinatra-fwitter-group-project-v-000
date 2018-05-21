78b63521591ec72167bb5174f56684e62da8a22b
Michelle Olivieri <michelleo72@gmail.com>
2018-05-21 19:16:41 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,52 @@
-class UserController < Sinatra::Base
+class UserController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
 
   get '/login' do
-    if Helper.logged_in?
+    if logged_in?
       redirect '/tweets'
     else
       erb :'/users/login'
     end
   end
 
-  get '/signup' do
-    if Helper.logged_in?
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect '/tweets'
     else
-      erb :'/users/signup'
+      redirect '/'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
     end
   end
 
